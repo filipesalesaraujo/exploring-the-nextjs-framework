dbacd427e244d89f06d77b9880e94a007dc40d3b
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-08-06 19:05:24 -0300

    Onde colocar meu CSS?

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Link from "next/link";
 import NextLink from "next/link";
+
+function Title({children, as}) {
+    const Tag = as;
+    return (
+        <>
+            <Tag>{children}</Tag>
+            <style jsx>{`
+              ${Tag} {
+                color: red;
+                font-family: sans-serif;
+              }
+            `}
+            </style>
+        </>
+    )
+}
 
 export default function HomePage() {
     return (
         <>
-            <h1>Alura Cases - Homepage</h1>
-           <NextLink href='/faq'>Ir para a página do FAQ</NextLink>
+            <Title as="h1">Alura Cases - Homepage</Title>
+            <NextLink href='/faq'>Ir para a página do FAQ</NextLink>
         </>
     )
 }
